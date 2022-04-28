c96275b0c1f843f45cd7e3b882b95123b7186dde
harshitsuthar77731 <65647274+harshitsuthar77731@users.noreply.github.com>
2022-04-28 14:39:56 +0530

    Create Longest Common Subsequence top down.cpp

---
+++ Longest Common Subsequence top down.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string x, string y) {
+        int n = x.size();
+        int m = y.size();
+        int dp[n+1][m+1];
+       
+        // initialization
+        for(int i=0;i<=n;i++)
+            dp[i][0] =0;
+        
+        for(int j=0;j<=m;j++)
+            dp[0][j] =0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(x[i-1]==y[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+        else if(x[i-1]!=y[j-1])
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]); 
+            }
+        }
+        return dp[n][m];
+    }
+};
