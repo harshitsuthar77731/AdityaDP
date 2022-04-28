a859e897986c982bebc6291d33ca7f1749750996
harshitsuthar77731 <65647274+harshitsuthar77731@users.noreply.github.com>
2022-04-28 14:32:47 +0530

    Create Longest Common Subsequence recursion + memoization.cpp

---
+++ Longest Common Subsequence recursion + memoization.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    Solution(){
+        memset(dp,-1,sizeof(dp));
+    }
+    int helper(string x, string y,int n,int m)
+    {
+        // base case ->if either string length became 0 there will be no common element b/w them
+        if(n==0||m==0)
+            return 0;
+        if(dp[n][m]!=-1)
+            return dp[n][m];
+        
+        // recursive case
+        if(x[n-1]==y[m-1])
+            return dp[n][m] = 1+helper(x,y,n-1,m-1); // we got one subsequence so adding 1 as result
+        else if(x[n-1]!=y[m-1])
+            return dp[n][m] = max(helper(x,y,n-1,m),helper(x,y,n,m-1)); // if we didn't got equal element one time we will check in reducing index of x and one by y where ever maximum result avilable return that
+        return 0;
+    }
+    int longestCommonSubsequence(string x, string y) {
+        return helper(x,y,x.size(),y.size());
+    }
+};
