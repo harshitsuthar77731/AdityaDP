095971317722706d90891f528067b2c9820fd176
harshitsuthar77731 <65647274+harshitsuthar77731@users.noreply.github.com>
2022-05-04 11:19:29 +0530

    Create Matric chain multiplication recursion + memoization.cpp

---
+++ Matric chain multiplication recursion + memoization.cpp
@@ -0,0 +1,29 @@
+class Solution{
+public:
+    int dp[101][101];
+    Solution()
+    {
+        memset(dp,-1,sizeof(dp));
+    }
+    int Solve(int arr[], int i,int j )
+    {
+        if(i>=j)
+            return 0;
+            
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+            
+        int ans = INT_MAX;
+        
+        for(int k=i;k<=j-1;k++)
+        {
+            int temp = Solve(arr,i,k)+Solve(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
+            ans = min(ans,temp);
+        }
+        return dp[i][j] = ans;
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        return Solve(arr,1,N-1);
+    }
+};
